818ac47b520abada0edbeb3c587e287ff1d28f38
maru1021 <130283016+maru1021@users.noreply.github.com>
2024-09-22 14:16:12 +0000

    t

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -23,6 +23,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_equal 'Welcome to the Sample App!', flash[:success]
+    assert is_logged_in?
   end
 end
