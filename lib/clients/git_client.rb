f6aaa2d25df6b2d1b3aacc3e91a1c8d95f820ba9
maru1021 <poprockguitarmaru@gmail.com>
2024-10-05 13:00:12 +0900

    test

--- lib/clients/git_client.rb
+++ lib/clients/git_client.rb
@@ -7,29 +7,31 @@ class GitClient
     `git describe --tags --abbrev=0`.strip
   end
 
-  # コミットログの取得（マージ元ブランチを含む）
+  # マージコミットを含むコミットログを取得
   def self.commits_since_latest_tag(branch)
     latest_tag = self.latest_tag
     commits = if latest_tag.empty?
-                `git log #{branch} --pretty=format:"%H %s" -n 10 --merges`.split("\n")
+                `git log #{branch} --pretty=format:"%H %s" -n 10`.split("\n")
               else
-                `git log #{latest_tag}..#{branch} --pretty=format:"%H %s" --merges`.split("\n")
+                `git log #{latest_tag}..#{branch} --pretty=format:"%H %s"`.split("\n")
               end
     commits.map { |commit| parse_commit(commit) }
   end
 
-  # マージ元ブランチ情報を取得するメソッド
+  # マージコミットの情報を解析
   def self.parse_commit(commit)
     hash, message = commit.split(' ', 2)
 
     if message.include?('Merge pull request')
-      # マージコミットの場合は、マージ元ブランチを取得
+      # マージコミットの場合、マージ元の2番目の親コミットを取得
       merge_info = `git log -1 --pretty=format:"%P" #{hash}`.split(' ')
-      parent_commit = merge_info[1] # 2番目の親コミットがマージ元
+      parent_commit = merge_info[1] # 2番目の親がマージ元
       merge_branch = `git name-rev --name-only #{parent_commit}`.strip
-      { hash:, message:, branch: merge_branch }
+      parent_message = `git log -1 --pretty=format:"%s" #{parent_commit}`.strip
+      parent_hash = `git log -1 --pretty=format:"%H" #{parent_commit}`.strip
+      { hash: parent_hash, message: parent_message, branch: merge_branch }
     else
-      { hash:, message:, branch: }
+      { hash:, message:, branch: current_branch }
     end
   end
 end
